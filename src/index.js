3ffd167dd6d59f3854e3747f1475a24fdca9f283
Adriano Gotardo <adrianogotardo.12@gmail.com>
2025-02-05 17:53:56 +0000

    Hospedei no AWS usando EC2 (o projeto é um servidor Express que exibe um arquivo estático em sua URL base), cadastrei um domínio (www.mymo.space) e configurei o DNS e um certificado SSL para que o acesso ocorra apropriadamente

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import router from './router.js';
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 const app = express();
 app.use(express.json());
@@ -13,6 +13,6 @@ app.use(router);
 app.use(express.static("static"));
 
 app.listen(+PORT, '0.0.0.0', () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
